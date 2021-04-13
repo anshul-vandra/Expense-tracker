ab0689028df3ad34d1dd4b5d00b4ed5c76938966
anshul.vandra <anshul.vandra@agileinfoways.com>
2021-04-13 15:23:40 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+//Header
+import Header from './components/Header'
+//Balance
+import Balance from './components/Balance'
+//css
+import './css/App.css'
+import IncomeExpense from './components/IncomeExpense'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <div className="container">
+        <Balance />
+        <div className="inc-exp-container">
+          <IncomeExpense />
+        </div>
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
